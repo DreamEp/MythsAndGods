07a964fb88b086b07aef44327da4161d11956be7
EtiennePinClaret <pinclaret@gmail.com>
2020-03-02 15:23:15 +0100

    Addind Title Bar

--- app/src/main/java/fr/esilv/mythsandgods/MainActivity.java
+++ app/src/main/java/fr/esilv/mythsandgods/MainActivity.java
@@ -1,5 +1,6 @@
 package fr.esilv.mythsandgods;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
